5df2d23ead0ca196fa55c37c9688b9fd9061ebfe
Milena Austel <maustel@1-C-10.42heilbronn.de>
2024-09-26 12:47:08 +0200

    superviser without datarace

--- philo/supervision.c
+++ philo/supervision.c
@@ -6,11 +6,28 @@
 /*   By: maustel <maustel@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 09:33:40 by maustel           #+#    #+#             */
-/*   Updated: 2024/09/19 16:07:41 by maustel          ###   ########.fr       */
+/*   Updated: 2024/09/26 12:45:52 by maustel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+// past - args->philos[i].last_meal_time > args->time_to_die
+// 				&& !args->philos[i].full
+
+static bool	philo_died(t_philo philo)
+{
+	long	past_time;
+	long	last_meal_time;
+
+	past_time = gettime_us(philo.args) - philo.args->start_simulation;
+	last_meal_time = get_long(philo.args,
+		philo.philo_mutex, philo.last_meal_time);
+	if (past_time - last_meal_time > philo.args->time_to_die)
+		return (true);
+	else
+		return (false);
+}
 
 /*
 	check if all threads are running (nbr_philos_ready)
@@ -49,8 +66,8 @@ void	*supervise_meal(void *ar)
 		while (i < args->nbr_philos && !simulation_finished(args))
 		{
 			past = gettime_us(args) - args->start_simulation;
-			if (past - args->philos[i].last_meal_time > args->time_to_die
-				&& !args->philos[i].full)
+			if (!get_bool(args, args->philos[i].philo_mutex, args->philos[i].full)
+				&& philo_died(args->philos[i]))
 				{
 					set_bool(args, args->args_mutex, &args->end_simulation, true);
 					print_status(args, args->philos[i], DIED);
